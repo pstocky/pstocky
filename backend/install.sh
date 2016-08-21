d85590b404f3c99a8c9a13a46431737b54852de2
Jackon Yang <i@jackon.me>
2016-08-21 23:54:54 +0800

    super user will be loaded by loaddata. no input required

--- backend/install.sh
+++ backend/install.sh
@@ -3,8 +3,8 @@
 rm -f db.sqlite3
 
 python manage.py migrate
-python manage.py createsuperuser \
-    --username=admin
+# python manage.py createsuperuser \
+#     --username=admin
 
 python manage.py loaddata init_data/initial_data.json
 
